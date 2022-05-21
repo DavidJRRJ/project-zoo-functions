13473c43e1267067aaa979b5f9b2190246aaf322
David Junior <david.lourenco94@gmail.com>
2022-05-21 12:55:44 -0300

    Quinto requisito Ok

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const especie = (animal) =>
+  data.species.find(({ name }) => name === animal).residents.length;
+const especieSex = (animal, sexo) =>
+  data.species
+    .find(({ name }) => name === animal)
+    .residents.filter(({ sex }) => sex === sexo).length;
+const todos = data.species.reduce((acc, curr) => {
+  acc[curr.name] = curr.residents.length;
+  return acc;
+}, {});
+
+function countAnimals(animal = {}) {
+  const { specie, sex } = animal;
+  if (sex) return especieSex(specie, sex);
+  if (specie) return especie(specie);
+  return todos;
 }
 
 module.exports = countAnimals;
