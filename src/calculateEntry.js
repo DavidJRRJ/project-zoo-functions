924f8f0b728db588e0aaaf3b3115680543b8ef5d
David Junior <david.lourenco94@gmail.com>
2022-05-21 21:39:00 -0300

    calculateEntry OK

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  return {
+    child: entrants.filter((crianca) => crianca.age < 18).length,
+    adult: entrants.filter((adulto) => adulto.age >= 18 && adulto.age < 50)
+      .length,
+    senior: entrants.filter((idoso) => idoso.age >= 50).length,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined) return 0;
+  if (Object.keys(entrants).length === 0) return 0;
+  const quant = countEntrants(entrants);
+  const senior = data.prices.senior * quant.senior;
+  const child = data.prices.child * quant.child;
+  const adult = data.prices.adult * quant.adult;
+  return senior + child + adult;
 }
 
 module.exports = { calculateEntry, countEntrants };
