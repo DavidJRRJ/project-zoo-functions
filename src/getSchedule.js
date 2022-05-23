2926c53876af38364613383b9237134f4fb9d195
David Junior <david.lourenco94@gmail.com>
2022-05-22 22:39:55 -0300

    correção erro lint e no avaliador

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,11 +1,5 @@
 const data = require('../data/zoo_data');
 
-// const animalsAv = data.species.find(({name}) => name === scheduleTarget).availability;
-/* const dontAnimal = data.species.map((elemento) => {
-  return elemento.name;
-  return elemento.availability;
-});  */
-
 function getSchedule(scheduleTarget) {
 
 }
