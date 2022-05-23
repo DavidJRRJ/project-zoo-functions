762e522ca2657ef9aaecbc50e22c67657396d000
David Junior <david.lourenco94@gmail.com>
2022-05-23 18:21:50 -0300

    Função GetSchedule lint Ok

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,5 @@
 const data = require('../data/zoo_data');
 
-console.log(data.employees.find(object => console.log(object)).responsibleFor.map((obj) => console.log(obj)));
-
 function getEmployeesCoverage() {
   // seu código aqui
 }
