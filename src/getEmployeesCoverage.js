63e5be0c32812fa27212b89b4e145ed16c97ae8e
David Junior <david.lourenco94@gmail.com>
2022-05-24 19:21:15 -0300

    Requisito 11 travado

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,15 +1,41 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-// const buscaNome = (nome) =>
-//   data.employees.find(({ firstName, id }) => firstName === nome || id === nome)
-//     .firstName;
-// const sname = (nome) =>
-//   data.employees.find(({ firstName, id }) => firstName === nome || id === nome)
-//     .lastName;
-// const animaisID = (nome) =>
-//   data.employees.find(({ firstName, id }) => firstName === nome || id === nome)
-//     .responsibleFor;
+const buscaNome = (nome) =>
+  employees.find(({ firstName, id }) => firstName === nome || id === nome)
+    .firstName;
 
-function getEmployeesCoverage(test) {}
+const sname = (nome) =>
+  employees.find(({ firstName, id }) => firstName === nome || id === nome)
+    .lastName;
+
+const location = (code) =>
+  employees
+    .find(({ id, firstName }) => id === code || firstName === code)
+    .responsibleFor.map(
+      (local) => species.find(({ id }) => id === local).location,
+    );
+
+const especies = (code) =>
+  employees
+    .find(({ id, firstName }) => id === code || firstName === code)
+    .responsibleFor.map((local) => species.find(({ id }) => id === local).name);
+
+const objeto = (value = {}) => {
+  // const { name, id } = value;
+  const obj = {
+    id: employees.find(({ firstName }) => firstName === value).id,
+    fullName: `${buscaNome(value)} ${sname(value)}`,
+    species: especies(value),
+    locations: location(value),
+  };
+  return obj;
+};
+
+function getEmployeesCoverage(value) {
+  return objeto(value);
+}
+
+console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 
 module.exports = getEmployeesCoverage;
