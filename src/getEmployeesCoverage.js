4f9220c7d85ac3d8c34cb5c062debc0dba80520e
David Junior <david.lourenco94@gmail.com>
2022-05-24 01:00:26 -0300

    Função get schedule sem objetos estáticos lint

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,12 +1,15 @@
 const data = require('../data/zoo_data');
 
-const buscaNome = (nome) => data.employees.find(({firstName, id}) => firstName === nome || id === nome).firstName;
-const sobrenome = (nome) => data.employees.find(({firstName, id}) => firstName === nome || id === nome).lastName;
-const animaisID = (nome) => data.employees.find(({firstName, id}) => firstName === nome || id === nome).responsibleFor;
+// const buscaNome = (nome) =>
+//   data.employees.find(({ firstName, id }) => firstName === nome || id === nome)
+//     .firstName;
+// const sname = (nome) =>
+//   data.employees.find(({ firstName, id }) => firstName === nome || id === nome)
+//     .lastName;
+// const animaisID = (nome) =>
+//   data.employees.find(({ firstName, id }) => firstName === nome || id === nome)
+//     .responsibleFor;
 
+function getEmployeesCoverage(test) {}
 
-function getEmployeesCoverage(test) {
-  
-}
-console.log(especies('e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 module.exports = getEmployeesCoverage;
